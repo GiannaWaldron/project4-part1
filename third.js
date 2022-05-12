c9475b512d67e53ebf049d03456366a80e8e0bd1
GiannaWaldron <waldg352@newschool.edu>
2022-05-12 02:52:48 -0400

    objects added

--- third.js
+++ third.js
@@ -1,22 +1,17 @@
 var bgcolors = ["", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""]
 var letter = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var bgimg = ["url(img/1.png)", "url(img/2.png)", "url(img/3.png)", "url(img/4.png)", "url(img/5.png)", "url(img/6.png)", "url(img/7.png)", "url(img/8.png)", "url(img/9.png)", "url(img/10.png)", "url(img/11.png)", "url(img/12.png)", "url(img/13.png)", "url(img/14.png)", "url(img/15.png)", "url(img/16.png)", "url(img/17.png)", "url(img/18.png)", "url(img/19.png)", "url(img/20.png)", "url(img/21.png)", "url(img/22.png)", "url(img/23.png)", "url(img/24.png)", "url(img/25.png)", "url(img/26.png)"];
-
+var bgimg = ["url(image/a3.png)", "url(image/b3.png)", "url(image/c3.png)", "url(img/4.png)", "url(img/5.png)", "url(img/6.png)", "url(img/7.png)", "url(img/8.png)", "url(img/9.png)", "url(img/10.png)", "url(img/11.png)", "url(img/12.png)", "url(img/13.png)", "url(img/14.png)", "url(img/15.png)", "url(img/16.png)", "url(img/17.png)", "url(img/18.png)", "url(img/19.png)", "url(img/20.png)", "url(img/21.png)", "url(img/22.png)", "url(img/23.png)", "url(img/24.png)", "url(img/25.png)", "url(img/26.png)"];
 document.onkeydown = function (e) {
   console.log("key down", event);
   document.querySelectorAll(".colors").forEach(a => {
     a.style.display = "none";
   })
-  document.body.style.backgroundColor = "#000000";
-  cursor.style.display = "block";
-  var randomIndex = Math.floor(Math.random() * bgcolors.length);
-  if (e.key == " ") { return };
+  document.body.style.backgroundColor = "#f8f6f6";
   cursor.style.display = "none";
-  document.body.style.backgroundColor = bgcolors[e.keyCode - 65];
-  document.body.style.backgroundImage = bgimg[randomIndex];
+  document.body.style.backgroundImage = bgimg[e.keyCode - 65];
   if (e.key == "Enter") { 
     document.getElementById(letter[randomIndex]).style.display = "block";
-    document.body.style.backgroundColor = bgcolors[randomIndex];
+    document.body.style.backgroundImage = backgroundImage[randomIndex];
   }
   else {
     document.getElementById(e.key).style.display = "block";
